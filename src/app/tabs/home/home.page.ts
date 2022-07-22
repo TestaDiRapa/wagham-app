beb6569f07bb0b583890ab150e6f935ef6640c5f
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-07-22 18:57:45 +0200

    Updated login to support OAuth flow on mobile devices

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Browser } from '@capacitor/browser';
 import { environment } from 'src/environments/environment';
 import { switchMap, take } from 'rxjs/operators';
@@ -6,68 +6,87 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { Router } from '@angular/router';
 import { WaghamLoadingController } from 'src/app/shared/wagham-loading-controller';
 import { UserType } from 'src/app/auth/token.model';
+import { InAppBrowser } from '@awesome-cordova-plugins/in-app-browser/ngx';
+import { Subscription } from 'rxjs';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
   readonly userType = UserType;
   userRole = null;
   logoPath = `${environment.waghamApi}/content/images/logo.png`;
+  private browserSubscription: Subscription = null;
 
   constructor(
     private authService: AuthService,
+    private iab: InAppBrowser,
     private loadingCtrl: WaghamLoadingController,
+    private platform: Platform,
     private router: Router
   ) {}
 
-  ngOnInit() {
-    this.loadingCtrl
-      .create()
-      .then((loadingElement) => {
-        loadingElement.present();
-        const urlParams = new URLSearchParams(location.search);
-        this.router.navigate([], {
-          queryParams: {
-            code: null,
-          },
-          queryParamsHandling: 'merge',
-        });
-        let tokenSubscription;
-        if (urlParams.has('code')) {
-          tokenSubscription = this.authService.login(urlParams.get('code'));
-        } else {
-          tokenSubscription = this.authService.token.pipe(
-            take(1),
-            switchMap((token) => {
-              if (!token) {
-                throw new Error('NO_TOKEN');
-              }
-              return token.role;
-            })
-          );
-        }
-        tokenSubscription.subscribe(
-          (role) => {
-            if (role !== UserType.newPlayer && role !== UserType.outsider) {
-              this.router.navigateByUrl('/tabs/character');
-            }
-            this.userRole = role;
-            loadingElement.dismiss();
-          },
-          () => {
-            loadingElement.dismiss();
-          }
-        );
-      });
+  ngOnDestroy(): void {
+    if (!!this.browserSubscription) {
+      this.browserSubscription.unsubscribe();
+    }
   }
 
-  ionViewWillEnter() {}
+  ngOnInit(): void {
+  }
 
-  discordAuth() {
-    Browser.open({ url: environment.discordAuthUrl, windowName: '_self' });
+  ionViewWillEnter(): void {
+    if (!this.platform.is('hybrid')) {
+      const params = new URLSearchParams(window.location.search);
+      if (params.has('code')) {
+        this.login(params.get('code'));
+      }
+    }
+  }
+
+  discordAuth(): void {
+    if (this.platform.is('hybrid')) {
+      if (!!this.browserSubscription) {
+        this.browserSubscription.unsubscribe();
+      }
+      const browser = this.iab.create(environment.discordAuthUrl);
+      this.browserSubscription = browser.on('loadstop').subscribe(event => {
+        if(event.url.startsWith('http://localhost')) {
+          const params = new URLSearchParams(new URL(event.url).search);
+          if (params.has('code')) {
+            browser.close();
+            this.login(params.get('code'));
+          }
+        }
+      });
+    } else {
+      Browser.open({ url: environment.discordAuthUrl, windowName: '_self' });
+    }
+  }
+
+  login(code: string) {
+    this.loadingCtrl.create().then((loadingElement) => {
+      loadingElement.present();
+      this.authService.login(code)
+      .pipe(
+        take(1),
+      )
+      .subscribe(
+        (token) => {
+          if (token.role !== UserType.newPlayer && token.role !== UserType.outsider) {
+            this.router.navigateByUrl('/tabs/character');
+          }
+          this.userRole = token.role;
+          loadingElement.dismiss();
+        },
+        () => {
+          loadingElement.dismiss();
+        }
+      );
+    });
   }
 
   joinWagham() {
